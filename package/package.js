20499e877cefa4f390e1a23af3e752373b60ab5e
Simon Schick <SimonSimCity@gmail.com>
2020-04-02 15:09:08 +0200

    Make `lmieulet:meteor-coverage` a weak dependency

--- package/package.js
+++ package/package.js
@@ -11,11 +11,11 @@ Package.onUse(function onUse(api) {
   api.use([
     'meteortesting:mocha-core@7.0.0',
     'ecmascript@0.3.0',
-    'lmieulet:meteor-coverage@1.1.4 || 2.0.1 || 3.0.0',
   ]);
 
   api.use(['meteortesting:browser-tests@1.3.2', 'http@1.0.0'], 'server');
   api.use('browser-policy@1.0.0', 'server', { weak: true });
+  api.use('lmieulet:meteor-coverage@1.1.4 || 2.0.1 || 3.0.0', 'client', { weak: true });
 
   api.mainModule('client.js', 'client');
   api.mainModule('server.js', 'server');
