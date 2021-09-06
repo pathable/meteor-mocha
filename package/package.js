a4721946e48f97fef1fc0ad8970953e33e4becf8
Nacho Codoñer <igcogi@gmail.com>
2021-09-06 18:48:44 +0200

    set compatibility with the new meteor-coverage version

--- package/package.js
+++ package/package.js
@@ -3,7 +3,7 @@ Package.describe({
   summary: 'Run Meteor package or app tests with Mocha',
   git: 'https://github.com/meteortesting/meteor-mocha.git',
   documentation: '../README.md',
-  version: '2.0.2',
+  version: '2.0.3',
   testOnly: true,
 });
 
@@ -15,7 +15,7 @@ Package.onUse(function onUse(api) {
 
   api.use(['meteortesting:browser-tests@1.3.2', 'http@1.0.0 || 2.0.0'], 'server');
   api.use('browser-policy@1.0.0', 'server', { weak: true });
-  api.use('lmieulet:meteor-coverage@1.1.4 || 2.0.1 || 3.0.0', 'client', { weak: true });
+  api.use('lmieulet:meteor-coverage@1.1.4 || 2.0.1 || 3.0.0 || 4.0.0', 'client', { weak: true });
 
   api.mainModule('client.js', 'client');
   api.mainModule('server.js', 'server');
